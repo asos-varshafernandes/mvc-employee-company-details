11a346cb1e54ec480ab0c11512d04e094032f7ba
Varsha Fernandes <varsha.fernandes@asos.com>
2023-03-28 17:26:14 +0100

    Update and Delete dto's added

--- MvcEmployeeCompanyDetails/MvcEmployeeCompanyDetails/Contracts/ICompanyRepository.cs
+++ MvcEmployeeCompanyDetails/MvcEmployeeCompanyDetails/Contracts/ICompanyRepository.cs
@@ -1,9 +1,14 @@
-﻿using MvcEmployeeCompanyDetails.Entities;
+﻿using MvcEmployeeCompanyDetails.Dto;
+using MvcEmployeeCompanyDetails.Entities;
 
 namespace MvcEmployeeCompanyDetails.Contracts
 {
     public interface ICompanyRepository
     {
         public Task<IEnumerable<Company>> GetCompanies();
+        public Task<Company> GetCompany(int id);
+        public Task<Company> CreateCompany(CompanyForCreationDto company);
+        public Task UpdateCompany(int id, CompanyForUpdateDto company);
+        public Task DeleteCompany(int id);
     }
 }
--- MvcEmployeeCompanyDetails/MvcEmployeeCompanyDetails/Repository/CompanyRepository.cs
+++ MvcEmployeeCompanyDetails/MvcEmployeeCompanyDetails/Repository/CompanyRepository.cs
@@ -1,6 +1,8 @@
-﻿using Dapper;
+﻿using System.Data;
+using Dapper;
 using MvcEmployeeCompanyDetails.Context;
 using MvcEmployeeCompanyDetails.Contracts;
+using MvcEmployeeCompanyDetails.Dto;
 using MvcEmployeeCompanyDetails.Entities;
 
 namespace MvcEmployeeCompanyDetails.Repository
@@ -31,6 +33,71 @@ namespace MvcEmployeeCompanyDetails.Repository
                 }
                 return new List<Company>();
 
+            }
+        }
+
+        public async Task<Company> GetCompany(int id)
+        {
+            var query = "SELECT * FROM Companies WHERE Id = @Id";
+
+            using (var connection = _context.CreateConnection())
+            {
+                var company = await connection.QuerySingleOrDefaultAsync<Company>(query, new { id });
+                return company; 
+            }
+        }
+
+        public async Task<Company> CreateCompany(CompanyForCreationDto company)
+        {
+            var query = "INSERT INTO Companies (Name, Address, Country) VALUES (@Name, @Address, @Country)"
+                        + "SELECT CAST(SCOPE_IDENTITY() as int)"; 
+
+            var parameters = new DynamicParameters();
+            parameters.Add("Name", company.Name, DbType.String);
+            parameters.Add("Address", company.Address, DbType.String);
+            parameters.Add("Country", company.Country, DbType.String);
+
+            using (var connection = _context.CreateConnection())
+            {
+                var id = await connection.QuerySingleAsync<int>(query, parameters);
+
+                var createdCompany = new Company
+                {
+                    Id = id,
+                    Name = company.Name,
+                    Address = company.Address,
+                    Country = company.Country,
+
+                };
+
+                return createdCompany;
+            }
+           
+        }
+
+        public async Task UpdateCompany(int id, CompanyForUpdateDto company)
+        {
+            var query = "UPDATE Companies SET Name = @Name, Address = @Address, Country = @Country WHERE Id = @Id";
+
+            var parameters = new DynamicParameters();
+            parameters.Add("Id", id, DbType.Int32);
+            parameters.Add("Name", company.Name, DbType.String);
+            parameters.Add("Address", company.Address, DbType.String);
+            parameters.Add("Country", company.Country, DbType.String);
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, parameters);
+            }
+        }
+
+        public async Task DeleteCompany(int id)
+        {
+            var query = "DELETE FROM Companies WHERE Id = @Id";
+
+            using (var connection = _context.CreateConnection())
+            {
+                await connection.ExecuteAsync(query, new{id});
             }
         }
     }
